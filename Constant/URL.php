fd921521b3da942bee75186937dd2d29c42a0fab
sonhoai <sonhoai272@gmail.com>
2018-05-31 12:12:24 +0700

    fix

--- Constant/URL.php
+++ Constant/URL.php
@@ -2,3 +2,4 @@
 define("API", 'http://localhost:4000/api/v1/');
 define("API_LOGIN", API."auth/login");
 define("API_REGISTER", API."auth/register");
+define("API_CONFIRM_EMAIL", API."auth/register");
